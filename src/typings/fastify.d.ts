4a9e09d5c4919ba64896c06004a60481c96ee18d
VasiletsFour <vasilets.seven@gmail.com>
2022-07-17 16:06:23 +0300

    test deploy

--- src/typings/fastify.d.ts
+++ src/typings/fastify.d.ts
@@ -1,6 +1,6 @@
-import "fastify";
+import "typings/fastify";
 
-declare module 'fastify' {
+declare module 'typings/fastify' {
     export interface FastifyInstance {
         config: {
             NODE_ENV: "development" | "production";
